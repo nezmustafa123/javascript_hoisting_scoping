0fdefa5987401e29c487a2d444efb40d4dd29ed4
Nez Mustafa <nezmustafa123@gmail.com>
2021-03-09 20:37:51 +0000

    javascript execution context

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+//code finished compiling
+//ready to be executed global execution context (top level code) not inside function
+
+
+const name = 'Nez';
+
+const first = () => {
+    let a = 1;
+    const b = second();
+    a = a + b;
+    return a;
+};
+
+function second() {
+    var c = 2;
+    return c
+    //function body only executed when called!
+}
+
+//execution context environment where javascript is executed
+//javascript always runs inside execution context
+
