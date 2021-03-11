9c2aeba6ef319e4dd3c1875b805795d844b45a9d
Nez Mustafa <nezmustafa123@gmail.com>
2021-03-11 21:47:08 +0000

    scope chain building

--- script.js
+++ script.js
@@ -37,3 +37,36 @@ function second() {
 //javascript has only one thread of execution can do one thing at a time
 //when returning from function execution context popped off call stack
 //call stack is like map for javascript engine
+
+//scoping how programs's variables are organized and acccessed where do variables live
+//lexical scoping  is controlled by placement of functions and blocks in the code
+// functton that is written inside other function has access to variables written inside parent function
+//scrop space or environment in which a certain variable is declared
+//global scope, function scop and block scope 
+//in functions variable enviornment which is stored in functions execution context 
+//scope of a variable reigion of code where certain variable can be accessed
+
+//building scope/scope chain
+
+const myName = 'Nez';
+
+function first(){
+    //first scope (function)
+    const age = 30;
+    
+    if(age >= 30) {
+        const decade = 3;
+        var millenial = true; 
+        
+    }
+    function second() {
+        //second scope (function)
+        const job = 'teacher';
+        
+        console.log(`%{myName} is a ${age} year old ${job}`);
+    }
+    
+    second();
+}
+
+first();
