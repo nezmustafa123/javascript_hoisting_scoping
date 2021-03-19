899120dbf66a8a2951c7fa1298010c873699490b
Nez Mustafa <nezmustafa123@gmail.com>
2021-03-19 12:04:24 +0000

    arguments object

--- arrowfunctions.js
+++ arrowfunctions.js
@@ -39,17 +39,13 @@ const nez = {
         
         const self = this;
           //outside function this keyword set to nez
-        const isMeillenial = function() {
+        const isMillenial = function() {
             
             //get around it using self create a variable set it qeual to this
            // through the scope chain javascript goes up the scope chain and gets the this keyword
-            console.log(self);
+            console.log(self); //this
             console.log(self.year >= 1981 && self.year <= 1996);
-        };
-        
-        
-        const isMeillenial = function() {
-            console.log(this.year >= 1981 && this.year <= 1996);
+            //true
         };
         
         
@@ -68,3 +64,125 @@ const nez = {
 
 nez.greet();
 nez.calcAge();
+
+
+
+
+
+// normal function inside method es6 method
+const nez = {
+    firstName: 'Nez',
+    year: 1990,
+    calcAge: function() {
+        console.log(this);
+        //this = nez
+        console.log(2037- this.year);
+        //function inside method
+//solution1
+//      const self = this;
+//       outside function this keyword set to nez
+//       const isMeillenial = function() {
+//            
+//       get around it using self create a variable set it qeual to this
+//       through the scope chain javascript goes up the scope chain and gets the this keyword
+//            console.log(self);
+//            console.log(self.year >= 1981 && self.year <= 1996);
+//        };
+//        
+        
+        //Solution 2
+        
+      const isMillenial = () => {
+            //in the arrow function the this keyword points to the object
+         
+            console.log(this);//this points to object
+            console.log(this.year >= 1981 && this.year <= 1996);
+          //returns true uses this keyword of parent scope
+        
+        };
+        //calling function again
+       // regular funciton call not attached to anything this keyword is undefined
+       //because its a regular function call
+        isMillenial();
+        
+    },
+    //arrow function will use this keyword of parent scope
+    greet: () => {
+        console.log(this);
+        console.log(`Hey ${this.firstName}`); 
+    },
+    //will print out hey matilda because this keyword points to window object 
+    //firstname property added to window firstName:'Matilda'
+    //never use var or arrow function as a method
+    //this.first name translates to window.firstName 
+};
+
+
+nez.greet();
+nez.calcAge();
+
+
+
+//functions get access to arguments keyword available in regular functions
+
+//arguments keyword
+const addExpr = function (a, b) {
+    console.log(arguments);
+    return a + b;
+    //will return array 2 and 5
+};
+
+addExpr(2,5);
+//can add more parameters to function than you specify
+//can loop over the aarguments array for example
+addExpr(2, 5, 6, 12);
+
+
+var addArrow = (a, b) => {
+    console.log(arguments);
+    return a + b;
+
+}
+
+addArrow(2,5,8);
+//doesn't exist in an arrow function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
